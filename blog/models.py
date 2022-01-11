9514d9f3b651a7d5e798626ddc10c8058829a547
Shin-sibainu <shincode0712@gmail.com>
2022-01-11 21:46:34 +0900

    mod

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-data_added"]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def frontpage(request):
+    posts = Post.objects.all()
+    return render(request, "blog/frontpage.html", {"posts": posts})
